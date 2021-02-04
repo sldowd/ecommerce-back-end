18372a2ba8032fa790006684a43d1d0fb9744ae4
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-04 12:39:01 -0500

    category get route

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,14 +2,30 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-      host: 'localhost',
-      dialect: 'mysql',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
-    });
+// const sequelize = process.env.JAWSDB_URL
+//   ? new Sequelize(process.env.JAWSDB_URL)
+//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+//       host: 'localhost',
+//       dialect: 'mysql',
+//       port: 3001,
+//       dialectOptions: {
+//         decimalNumbers: true,
+//       },
+//     });
 
+// let sequelize;
+
+// if (process.env.JAWSDB_URL) {
+//   sequelize = new Sequelize(process.env.JAWSDB_URL);
+// }
+// } else {
+  const sequelize = new Sequelize('ecommerce_db', 'root', 'hotdog666', {
+    host: 'localhost',
+    dialect: 'mysql',
+    port: 3306,
+    dialectOptions: {
+      decimalNumbers: true,
+    },
+  });
+//}
 module.exports = sequelize;
--- Develop/models/Product.js
+++ Develop/models/Product.js
@@ -15,7 +15,7 @@ Product.init(
       primaryKey: true,
       autoIncrement: true
     },
-    procuct_name: {
+    product_name: {
       type: DataTypes.STRING,
       allowNull: false
     },
