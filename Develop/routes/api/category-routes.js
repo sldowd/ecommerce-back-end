a90fb8776c6fe9f6ce8f2f500d2775fa2b7f8461
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-04 13:15:19 -0500

    category post / put

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -39,7 +39,8 @@ router.get('/:id', (req, res) => {
   })
   .then(dbCategoryData => {
     if (!dbCategoryData) {
-      res.status(404).json({ message: 'There is no category associated with this id'})
+      res.status(404).json({ message: 'There is no category associated with this id.'})
+      return;
     }
     res.json(dbCategoryData)
   })
@@ -51,10 +52,34 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new category
+  Category.create({
+    category_name: req.body.category_name
+  })
+  .then(dbCategoryData => res.json(dbCategoryData))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
+  Category.update(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'There is no category associated with this id.' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 router.delete('/:id', (req, res) => {
