af3d2b8916688ddecb8a0d8ae46bb0da5d968509
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-04 14:35:37 -0500

    category delete

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -84,6 +84,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete a category by its `id` value
+  Category.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategoryData => {
+    if (!dbCategoryData) {
+      res.status(404).json({ message: 'There is no category associated with this id.' });
+      return;
+    }
+    res.json(dbCategoryData);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  })
 });
 
 module.exports = router;
