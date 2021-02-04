c82e6f197bb926f7d1ab8241134ec385f5f7a352
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-03 19:24:52 -0500

    defined associations for models

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,12 +5,28 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category, {
+  foreignKey: 'id'
+})
 
 // Categories have many Products
+Category.hasMany(Product, {
+  foreignKey: 'id'
+})
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  as: 'tag',
+  foreignKey: 'id'
+})
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  as: 'product',
+  foreignKey: 'id'
+})
 
 module.exports = {
   Product,
