a0813cddf104a9136d1e7ed2bae76b83b636d978
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-04 12:42:21 -0500

    fixed bug in associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,26 +6,26 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'id'
+  foreignKey: 'category_id'
 })
 
 // Categories have many Products
 Category.hasMany(Product, {
-  foreignKey: 'id'
+  foreignKey: 'category_id'
 })
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: ProductTag,
   as: 'tag',
-  foreignKey: 'id'
+  foreignKey: 'tag_id'
 })
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: ProductTag,
   as: 'product',
-  foreignKey: 'id'
+  foreignKey: 'product_id'
 })
 
 module.exports = {
