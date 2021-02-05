f8ad84e2c46b9267a2ed49d098eeb613a4cd330d
Sarah Dowd <sarahlynnedowd@gmail.com>
2021-02-05 08:14:43 -0500

    debug

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,25 +1,13 @@
+const Sequelize = require('sequelize');
 require('dotenv').config();
 
-const Sequelize = require('sequelize');
 
-// const sequelize = process.env.JAWSDB_URL
-//   ? new Sequelize(process.env.JAWSDB_URL)
-//   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
-//       host: 'localhost',
-//       dialect: 'mysql',
-//       port: 3001,
-//       dialectOptions: {
-//         decimalNumbers: true,
-//       },
-//     });
+let sequelize;
 
-// let sequelize;
-
-// if (process.env.JAWSDB_URL) {
-//   sequelize = new Sequelize(process.env.JAWSDB_URL);
-// }
-// } else {
-  const sequelize = new Sequelize('ecommerce_db', 'root', 'hotdog666', {
+if (process.env.JAWSDB_URL) {
+  sequelize = new Sequelize(process.env.JAWSDB_URL);
+} else {
+  sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
     host: 'localhost',
     dialect: 'mysql',
     port: 3306,
@@ -27,5 +15,7 @@ const Sequelize = require('sequelize');
       decimalNumbers: true,
     },
   });
-//}
+}
+
+
 module.exports = sequelize;
